a0cfa24b011abe3ed32a34dc190450c7235dfeef
J-Kiri <joaogufilho@gmail.com>
2024-07-14 16:59:32 -0300

    for now, ip and port and messege are hardcoded

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ try:
 
     server_ip = 'localhost'
     server_port = 8484
+    buffer_size = 1500
 
     address = (server_ip, server_port)
     while True:
@@ -12,7 +13,7 @@ try:
         byte_message = message.encode(encoding="utf-8")
         clientSocket.sendto(byte_message, address)
         
-        data, server = clientSocket.recvfrom(1500)
+        data, server = clientSocket.recvfrom(buffer_size)
         print("Received from ", server_ip, ": ", data.decode(encoding="utf-8"))
 
         if message == "CLOSE":
