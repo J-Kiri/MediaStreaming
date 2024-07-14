8361e3491b5af9c8409d6293ff5019051d5c9a32
J-Kiri <joaogufilho@gmail.com>
2024-07-14 14:40:46 -0300

    Basic server operation

--- server.py
+++ server.py
@@ -1,19 +1,29 @@
 import socket
 
 try:
+    buffer_size = 1500
+    
+    #Criação do server
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    #connect() apenas para pegar o IP da máquina
     s.connect(('8.8.8.8', 1))
     addresServer = s.getsockname()[0]
+    s.close()
 
-    print(addresServer)
-
-    server_ip = "127.0.0.1"
+    server_ip = addresServer
     server_port = 8000
 
-    serverSocket.bind((server_ip, server_port)) 
+    #Cria vinculo do socket com o IP e a porta
+    serverSocket.bind((server_ip, server_port))     
+    print("---------------- Server started! ---------")  
+    print("Listening on IP: ", server_ip, " Port: ", str(server_port))
+
+    while True:
+        data, address = serverSocket.recvfrom(buffer_size)
+        print("received message: ", data, "from: ", address)
+
 except Exception as e:
     print("Error: ", e)
-    
-    
+
