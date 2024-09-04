8785375493a7d858eef87baacb438883135699d5
raelz2 <rael.z2.cw@gmail.com>
2024-09-03 22:45:27 -0300

    thread criada para a passagem ao player de video

--- client.py
+++ client.py
@@ -4,88 +4,78 @@ from sys import stdout
 import sys
 import logging
 
-# SERVER_IP define o endereço IP do servidor ao qual o cliente se conectará.
-# UDP_PORT é a porta utilizada para receber os dados via UDP.
-# TCP_PORT é a porta utilizada para comunicação de controle via TCP.
-# BUFFER_SIZE define o tamanho do buffer para a leitura dos pacotes de dados.
-
 SERVER_IP = "localhost"
 UDP_PORT = 5005
 TCP_PORT = 5006
 NEW_TCP_PORT = 5007
 BUFFER_SIZE = 1400
 
-# Configuração básica de logging para registrar informações, erros e mensagens importantes no cliente.
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Função responsável por receber o stream de vídeo via UDP.
 def receive_udp_stream():
     try:
-        # Criação do socket UDP para receber os dados do vídeo.
         udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         address = (SERVER_IP, UDP_PORT)
-        udp_sock.sendto(b'ola', address)  # Envia uma mensagem inicial para o servidor, indicando que está pronto para receber dados.
+        udp_sock.sendto(b'ola', address)
         
-        id = 0  # Contador de pacotes recebidos.
-        video_data = bytearray()  # Armazena temporariamente os dados do vídeo recebidos.
+        id = 0
+        video_data = bytearray()
 
         while True:
-            data, _ = udp_sock.recvfrom(BUFFER_SIZE)  # Recebe um pacote UDP do servidor.
+            data, _ = udp_sock.recvfrom(BUFFER_SIZE)
             if not data:
-                break  # Se o pacote estiver vazio, significa que a transmissão terminou.
+                break
 
-            video_data += data  # Acumula os dados do vídeo no buffer.
+            video_data += data
             id += 1        
 
-            # A cada 10 pacotes recebidos, os dados são processados e enviados para a saída padrão (stdout).
-            if id == 10:
+            if id >= 10:
                 for i in range(0, len(video_data), BUFFER_SIZE):
                     stdout.buffer.write(video_data[i:i+BUFFER_SIZE])
                     sys.stdout.buffer.flush()
-                id = 0  # Reseta o contador de pacotes.
-                video_data = bytearray()  # Limpa o buffer de vídeo após o envio dos dados.
-                tcp_sock.send(b'NEXT')  # Envia uma mensagem TCP para o servidor, indicando que o cliente está pronto para o próximo lote de pacotes.
+                id = 0
+                video_data = bytearray()
+                tcp_sock.send(b'NEXT')
             
-        udp_sock.close()  # Fecha o socket UDP ao final da transmissão.
+        udp_sock.close()
     except Exception as e:
         logging.error(f"Error in streaming the video: {e}")
 
-# Função responsável por estabelecer a conexão de controle via TCP com o servidor.
 def send_tcp_control():
     try:
-        global tcp_sock  # Define tcp_sock como uma variável global para ser usada em outras partes do código.
+        global tcp_sock
         tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tcp_sock.connect((SERVER_IP, TCP_PORT))  # Conecta-se ao servidor na porta TCP definida.
+        tcp_sock.connect((SERVER_IP, TCP_PORT))
     except Exception as e:
         logging.error(f"Error in connecting TCP: {e}")
 
 def seek_control():
-    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_sock.connect((SERVER_IP, NEW_TCP_PORT))
-    print("Conexão de controle TCP estabelecida.")
-
-    while True:#p: pausar, c: continuar, s: sair
-        command = input()
-        tcp_sock.send(command.encode())
-        if command == "STOP":
-            break
-
-    tcp_sock.close()
-
-# Bloco principal do código, executado quando o script é iniciado.
-if __name__ == "__main__":
-    # Estabelece a conexão TCP para controle de fluxo.
-    send_tcp_control()
-    
-    seek_thread = threading.Thread(target=seek_control)
-    seek_thread.start()
-
     try:
-        # Cria uma thread separada para receber o stream de vídeo via UDP, permitindo que o cliente continue responsivo.
-        udp_thread = threading.Thread(target=receive_udp_stream)
-        udp_thread.start()
-        udp_thread.join()  # Aguarda até que a thread UDP termine antes de continuar.
-    except Exception as e:
-        logging.error(f"Error creating UDP thread: {e}")
+        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sock.connect((SERVER_IP, NEW_TCP_PORT))
+        logging.info("Conexão de controle TCP estabelecida.")
+
+        while True:#p: pausar, c: continuar, s: sair
+            command = input()
+            tcp_sock.send(command.encode())
+            if command == "STOP":
+                break
+
+    finally:
+        tcp_sock.close()
+
+if __name__ == "__main__":
+    try:
+        send_tcp_control()
         
-    tcp_sock.close()  # Fecha a conexão TCP ao final da transmissão.
+        seek_thread = threading.Thread(target=seek_control)
+        seek_thread.start()
+
+        try:
+            udp_thread = threading.Thread(target=receive_udp_stream)
+            udp_thread.start()
+            udp_thread.join()
+        except Exception as e:
+            logging.error(f"Error creating UDP thread: {e}")
+    finally:
+        tcp_sock.close()
--- server.py
+++ server.py
@@ -5,72 +5,58 @@ import logging
 import queue
 import threading
 
-# OWN_IP define o endereço IP local da máquina que executa o servidor. Aqui está configurado para 'localhost', mas pode ser substituído pelo IP apropriado.
-# UDP_PORT é a porta utilizada para comunicação via UDP.
-# TCP_PORT é a porta utilizada para comunicação via TCP.
-# BUFFER_SIZE define o tamanho do buffer para leitura dos dados do vídeo.
-
-OWN_IP = "localhost"
+OWN_IP = "192.168.0.107"
 UDP_PORT = 5005
 TCP_PORT = 5006
 NEW_TCP_PORT = 5007
 BUFFER_SIZE = 1400
 
+ROLLBACK_SECONDS = 10
+
 control_queue = queue.Queue()
 
-# Configuração básica de logging para registrar informações, erros e mensagens importantes do servidor.
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Função responsável por enviar o vídeo via UDP. Recebe o socket UDP, a conexão TCP, e o endereço do cliente.
 def handle_udp_streaming(udp_sock, tcp_conn, addr):
-    print("UDP Server Ready to send data...")
+    id = 0
+    total = 0
 
-    id = 0  # Contador de pacotes enviados.
-    total = 0  # Total de dados enviados em MB.
-
-    # Abrindo o arquivo de vídeo em modo binário para leitura.
-    with open('foguete.mp4', 'rb') as video_file:
+    with open('video.mp4', 'rb') as video_file:
         while True:
             try:
                 
-                if not control_queue.empty():
+                """if not control_queue.empty():
                     command = control_queue.get()
                     if command == "p":
-                        print("Streaming pausado.")
+                        logging.info("Streaming pausado.")
                         while control_queue.get() != "c":
                             pass
-                        print("Streaming retomado.")
+                        logging.info("Streaming retomado.")
                     elif command == "s":
-                        print("Streaming interrompido.")
+                        logging.info("Streaming interrompido.")
                         break
-                    elif command == "r":
-                        total -=100*BUFFER_SIZE
-                        video_file.seek(total)
+"""
                 
-                # Leitura de um pedaço do arquivo de vídeo com tamanho definido pelo BUFFER_SIZE.
                 data = video_file.read(BUFFER_SIZE)
                 if not data:
-                    # Se não houver mais dados para ler, significa que o vídeo terminou.
-                    print("End of video file")
-                    udp_sock.sendto(b'', addr)  # Envia um pacote vazio para indicar o fim do vídeo.
+                    logging.info("End of video file")
+                    udp_sock.sendto(b'', addr)
                     return
                
                 try:
                     id += 1
-                    total += len(data)  # Calcula o total de dados enviados em KB.
-                    udp_sock.sendto(data, addr)  # Envia o pacote UDP com os dados do vídeo.
-                    print(f"Sent {id} packets with {len(data)} bytes to {addr}, total sent: {total}B")
-                    time.sleep(0.0005)  # Pequena pausa para evitar congestionamento.
+                    total += len(data)
+                    udp_sock.sendto(data, addr)
+                    time.sleep(0.0005)
                 except Exception as e:
                     logging.error(f"Error in transmitting the video: {e}")
-
-                # A cada 10 pacotes, o servidor espera uma resposta do cliente via TCP.
-                if id == 10:
+                var = 0.2
+                if id == 10*(1 - var):
                     control_data = tcp_conn.recv(BUFFER_SIZE).decode()
-                    print(f"Received from client: {control_data}")
+                    logging.info(f"Received from client: {control_data}")
                     if control_data != "NEXT":
-                        break  # Se a resposta não for "NEXT", interrompe o envio.
-                    id = 0  # Reseta o contador de pacotes.
+                        break
+                    id = 0 
 
             except Exception as e:
                 logging.error(f"Error sending UDP data: {e}")
@@ -78,74 +64,67 @@ def handle_udp_streaming(udp_sock, tcp_conn, addr):
 
     return
 
-# Função responsável por estabelecer a conexão TCP com o cliente.
 def handle_tcp_control(tcp_sock):
     try:
-        tcp_sock.listen(1)  # Coloca o socket TCP em modo de escuta para aceitar conexões.
-        conn, addr = tcp_sock.accept()  # Aceita a conexão TCP do cliente.
-        print(f"TCP connection established with {addr}")
-        return conn, addr  # Retorna o objeto da conexão e o endereço do cliente.
+        tcp_sock.listen(1)
+        conn, addr = tcp_sock.accept()
+        logging.info(f"TCP connection established with {addr}")
+        return conn, addr
     except Exception as e:
         logging.error(f"Error establishing TCP connection: {e}")
-
+"""
 def seek_control():
     tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_sock.bind((OWN_IP, NEW_TCP_PORT))
     tcp_sock.listen(1)
     conn, addr = tcp_sock.accept()
-    print(f"Conexão de controle estabelecida com {addr}")
+    logging.info(f"Conexão de controle estabelecida com {addr}")
 
     while True:
         data = conn.recv(BUFFER_SIZE).decode()
         if not data:
             break
-        print(f"Comando recebido: {data}")
+        logging.info(f"Comando recebido: {data}")
         control_queue.put(data)
 
     conn.close()
     tcp_sock.close()
-
-
-# Função para encontrar uma porta livre no sistema, caso UDP_PORT ou TCP_PORT não sejam definidos.
+"""
 def find_free_port():
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('', 0))  # Liga o socket a uma porta livre.
+        s.bind(('', 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]  # Retorna o número da porta livre.
-
-# Bloco principal do código, executado quando o script é iniciado.
+        return s.getsockname()[1]
+    
 if __name__ == "__main__":
-    if(UDP_PORT and TCP_PORT == 0):
-        # Se as portas não forem definidas, encontra portas livres automaticamente.
-        UDP_PORT = find_free_port()
-        TCP_PORT = find_free_port()
-        print("UDP PORT =", UDP_PORT)
-        print("TCP PORT =", TCP_PORT)
-
-    seek_thread = threading.Thread(target=seek_control)
-
-    seek_thread.start()
-
     try:
-        # Cria o socket TCP e faz a ligação com o endereço e porta definidos.
-        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tcp_sock.bind((OWN_IP, TCP_PORT))
-        tcp_conn, tcp_addr = handle_tcp_control(tcp_sock)
-    except Exception as e:
-        logging.error(f"Error binding TCP: {e}") 
+        if(UDP_PORT and TCP_PORT == 0):
+            UDP_PORT = find_free_port()
+            TCP_PORT = find_free_port()
+            logging.info("UDP PORT =", UDP_PORT)
+            logging.info("TCP PORT =", TCP_PORT)
 
-    try:
-        # Cria o socket UDP e faz a ligação com o endereço e porta definidos.
-        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-        udp_sock.bind((OWN_IP, UDP_PORT))
-        _, addr = udp_sock.recvfrom(10)  # Espera uma mensagem inicial do cliente para obter o endereço.
-    except Exception as e:
-        logging.error(f"Error binding UDP: {e}")
+        #seek_thread = threading.Thread(target=seek_control)
 
-    # Inicia o envio dos dados de vídeo via UDP.
-    handle_udp_streaming(udp_sock, tcp_conn, addr)
+        #seek_thread.start()
 
-    # Fecha as conexões TCP e UDP ao final da transmissão.
-    tcp_conn.close()
-    tcp_sock.close()
-    print("Server closed")
+        try:
+            tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp_sock.bind((OWN_IP, TCP_PORT))
+            tcp_conn, tcp_addr = handle_tcp_control(tcp_sock)
+        except Exception as e:
+            logging.error(f"Error binding TCP: {e}") 
+
+        try:
+            udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+            udp_sock.bind((OWN_IP, UDP_PORT))
+            _, addr = udp_sock.recvfrom(10)
+        except Exception as e:
+            logging.error(f"Error binding UDP: {e}")
+
+        handle_udp_streaming(udp_sock, tcp_conn, addr)
+
+    finally:
+        tcp_conn.close()
+        tcp_sock.close()
+        logging.info("Server closed")
